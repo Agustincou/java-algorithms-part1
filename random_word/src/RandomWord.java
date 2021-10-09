57324c07b69e8b7ed104d4e05f95262d9f5bb201
Juan Agustín Coutinho <cou_647@hotmail.com>
2021-10-09 11:30:37 -0300

    fixes

--- random_word/src/RandomWord.java
+++ random_word/src/RandomWord.java
@@ -2,12 +2,15 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
-    static public void main(String[] args) {
+    public static void main(String[] args) {
         double wordCounter = 1;
         String championWord = "";
-        while (!StdIn.isEmpty() && StdRandom.bernoulli(1 / wordCounter)) {
-            championWord = StdIn.readString();
-            ++wordCounter;
+        while (!StdIn.isEmpty()) {
+            String wordRead = StdIn.readString();
+            if (StdRandom.bernoulli(1 / wordCounter)) {
+                championWord = wordRead;
+                ++wordCounter;
+            }
         }
         System.out.print(championWord);
     }
